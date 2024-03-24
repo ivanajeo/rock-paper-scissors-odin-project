32935dc3e0f699af5663bf37bda84c0c3e1f584e
Van <152158486+ivanajeo@users.noreply.github.com>
2024-03-24 12:54:43 +0700

    Refactor function playRound for better readability

--- game.js
+++ game.js
@@ -10,42 +10,39 @@ function getComputerChoice() {
     }
 }
 
-
 function playRound(playerSelection, computerSelection) {
     playerSelection = playerSelection.toLowerCase();
     let winCount;
-    if (computerSelection === "rock" && playerSelection === "rock") {
-        alert("TIE!");
-        winCount += 0;
-    } else if (computerSelection === "rock" && playerSelection === "paper") {
-        alert("You Win! Paper beats Rock");
-        winCount += 1;
-    } else if (computerSelection === "rock" && playerSelection === "scissors") {
-        alert("You Lose! Rock beats Scissors");
-        winCount += 0;
 
-    } else if (computerSelection === "paper" && playerSelection === "rock") {
-        alert("You Lose! Paper beats Rock");
-        winCount += 0;
-    } else if (computerSelection === "paper" && playerSelection === "paper") {
+    if (computerSelection === playerSelection) {
         alert("TIE!");
         winCount += 0;
-    } else if (computerSelection === "paper" && playerSelection === "scissors") {
-        alert("You Win! Scissors beats Paper");
-        winCount += 1;
-        
-    } else if (computerSelection === "scissors" && playerSelection === "rock") {
-        alert("You Win! Rock beats Scissors");
-        winCount += 1;
-    } else if (computerSelection === "scissors" && playerSelection === "paper") {
-        alert("You Lose! Scissors beats Paper");
-        winCount += 0;
-    } else if (computerSelection === "scissors" && playerSelection === "scissors") {
-        alert("TIE!");
-        winCount += 0;
+    } else if (computerSelection === "rock") {
+        if (playerSelection === "paper") {
+            alert("You Win! Paper beats Rock");
+            winCount += 1;
+        } else if (playerSelection === "scissors") {
+            alert("You Lose! Rock beats Scissors");
+            winCount += 0;
+        }
+    } else if (computerSelection === "paper") {
+        if (playerSelection === "rock") {
+            alert("You Lose! Paper beats Rock");
+            winCount += 0;
+        } else if (playerSelection === "scissors") {
+            alert("You Win! Scissors beats Paper");
+            winCount += 1;
+        }
+    } else if (computerSelection === "scissors") {
+        if (playerSelection === "rock") {
+            alert("You Win! Rock beats Scissors");
+            winCount += 1;
+        } else if (playerSelection === "paper") {
+            alert("You Lose! Scissors beats Paper");
+            winCount += 0;
+        }
     }
 }
-
 
 function playGame() {
     for (let i = 0; i < 5; i++) {
