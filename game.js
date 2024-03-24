2cf3345ce4414b3dbd9109334cbc3a4fff6614e9
Van <152158486+ivanajeo@users.noreply.github.com>
2024-03-24 10:07:34 +0700

    Add function playRound and prompt getPlayerChoice

--- game.js
+++ game.js
@@ -2,12 +2,38 @@ function getComputerChoice() {
     const randomNumber = Math.floor(Math.random() * 3);
     switch (randomNumber) {
         case 0:
-            return 'rock';
+            return "rock";
         case 1:
-            return 'paper';
+            return "paper";
         case 2:
-            return 'scissors';
+            return "scissors";
     }
 }
 
-getComputerChoice();
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    
+    if (computerSelection === "rock" && playerSelection === "rock") {
+        alert("TIE!");
+    } else if (computerSelection === "rock" && playerSelection === "paper") {
+        alert("You Win! Paper beats Rock");
+    } else if (computerSelection === "rock" && playerSelection === "scissors") {
+        alert("You Lose! Rock beats Scissors");
+    } else if (computerSelection === "paper" && playerSelection === "rock") {
+        alert("You Lose! Paper beats Rock");
+    } else if (computerSelection === "paper" && playerSelection === "paper") {
+        alert("TIE!");
+    } else if (computerSelection === "paper" && playerSelection === "scissors") {
+        alert("You Win! Scissors beats Paper");
+    } else if (computerSelection === "scissors" && playerSelection === "rock") {
+        alert("You Win! Rock beats Scissors");
+    } else if (computerSelection === "scissors" && playerSelection === "paper") {
+        alert("You Lose! Scissors beats Paper");
+    } else if (computerSelection === "scissors" && playerSelection === "scissors") {
+        alert("TIE!");
+    }
+}
+
+let getPlayerChoice = prompt("Rock, Paper, Scissors?");
+
+playRound(getPlayerChoice, getComputerChoice());
